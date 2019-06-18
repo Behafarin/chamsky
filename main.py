50364f296762e0caab92118898349a1d25eb63ff
Behafarin <44119795+Behafarin@users.noreply.github.com>
2019-06-18 15:49:56 +0430

    beautiful but fucked up

--- main.py
+++ main.py
@@ -1,36 +1,62 @@
 import re
+
+
+class Chamsky:
+
+    def __init__(self, txt):
+        self.txt = txt
+
+    def make_T(self):
+        T = {}
+        terminals = re.findall('[a-z]', txt)
+        terminals = list(set(terminals))
+        for i in terminals:
+            T["T" + i] = i
+        return T
+
+    def print_grammer(self, left_part, right_part):
+        for i in right_part:
+            if i != '' and i != left_part:
+                print(left_part+'->'+i)
+        T = self.make_T()
+        for i, j in T.items():
+            print(i+'->'+j)
+
+    def chamsky(self):
+        grammer = re.split('->', self.txt, 1)
+        left_part = grammer[0]
+        right_part = grammer[1].split('|')
+        flag = False
+        temp = None
+        add_list = []
+        for i in right_part:
+            if i == '#':
+                right_part.remove(i)
+                flag = True
+        if flag:
+            for i in right_part:
+                i = i.replace(left_part, '')
+                add_list.append(i)
+
+        for i in right_part:
+            if i.isupper() and len(i) == 1:
+                temp = i
+                right_part.remove(i)
+
+        if temp is not None:
+            for i in right_part:
+                i = i.replace(left_part, temp)
+                add_list.append(i)
+#        for i in right_part:
+#            for j in range(0, len(i)):
+#                if i[j].islower():
+#                    i = i.replace(i[j], "T"+i[j])
+#                    add_list.append(i)
+        right_part.extend(add_list)
+        right_part = list(set(right_part))
+        self.print_grammer(left_part, right_part)
+
+
 txt = input()
-T = {}
-# finding lowercase
-x = re.findall('[a-z]', txt)
-x = list(dict.fromkeys(x))
-y = re.split("->", txt, 1)
-z = re.split("\|", y[1])
-flag = False
-temp = None
-for i in z:
-    if i == '#':
-        z.remove(i)
-        flag = True
-if flag:
-    for i in z:
-        for j in range(0, len(i)):
-            if i[j] == y[0]:
-                z.append(i[0:j]+i[j+1:len(i)])
-for i in z:
-    if i.isupper() and len(i) == 1:
-        temp = i
-        z.remove(i)
-if temp is not None:
-    for i in z:
-        for j in range(0, len(i)):
-            if i[j] == y[0]:
-                z.append(i[0:j]+temp+i[j+1:len(i)])
-z = list(dict.fromkeys(z))
-for i in x:
-    T["T"+i] = i
-for i in z:
-    if i != '' and i != y[0]:
-        print(y[0]+'->'+i)
-for i, j in T.items():
-    print(i+'->'+j)
+hey = Chamsky(txt)
+hey.chamsky()
